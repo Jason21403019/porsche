167bc107cdc2cff824c787a560f0cf8bd84126f2
DESKTOP-47A4PN7\1 <jason86100110@gmail.com>
2025-02-17 17:31:23 +0800

    車子輪播開始。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     optimizeDeps: {
-      include: ["gsap"],
+      include: ['gsap'],
     },
   },
-});
+})
